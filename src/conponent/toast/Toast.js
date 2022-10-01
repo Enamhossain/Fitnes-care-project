64bebb12a770a4355dd9f3bb05d2cddc4d6d46a8
Enamhossain <bhaianam9@gmail.com>
2022-10-02 00:37:59 +0600

    project done

--- src/conponent/toast/Toast.js
+++ src/conponent/toast/Toast.js
@@ -5,8 +5,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
   
   const Toest = () => {
-   let  notify = () => toast("congratulation,You are complate All Task !");
-      
+   let  notify = () => toast("congratulation,You are complate All Task !");  
     return (
       <div>
         <button onClick={notify}className="btn btn-dark p-3">
